444513dba7bb25c36e188d803c458d9867dcca74
Securti <41710043+Securti@users.noreply.github.com>
2020-06-05 22:36:38 +0900

    Add files via upload

---
+++ Gamemode.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @name Gamemode
+ * @main securti\gamemode\Gamemode
+ * @author ["Securti"]
+ * @version 0.1
+ * @api 3.13.0
+ * @description License : LGPL 3.0
+ */
+ 
+namespace securti\gamemode;
+ 
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\entity\Entity;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+
+use pocketmine\level\Level;
+
+class Gamemode extends PluginBase implements Listener{
+
+  public $list = array("world" => 0, "island" => 0);
+  
+  public $mode = 1; //0 : apply to op, 1 : apply to user(except op)
+  
+  public static $instance;
+  
+  public static function getInstance(){
+  
+    return self::$instance;
+  }
+  public function onLoad(){
+  
+    self::$instance = $this;
+  }
+  public function onEnable(){
+  
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onJoin(PlayerJoinEvent $e){
+  
+    $player = $e->getPlayer();
+    
+    $level = $player->getLevel();
+    
+    $this->setGamemode($player, $level);
+  }
+  public function onEntityLevelChange(EntityLevelChangeEvent $e){
+  
+    $entity = $e->getEntity();
+    
+    if($entity instanceof Player){
+    
+      $level = $e->getTarget();
+      
+      $this->setGamemode($entity, $level);
+    }
+  }
+  public function setGamemode(Player $player, Level $level){
+  
+    $list = $this->list;
+    $mode = $this->mode;
+    
+    $level_name = $level->getFolderName();
+    
+    if($player->isOp() and $mode == 1) return true;
+    
+    if(isset($list[$level_name])){
+    
+      $int = $list[$level_name];
+      
+      $player->setGamemode($int);
+    }
+  }
+}
